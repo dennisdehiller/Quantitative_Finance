bdb0b83a5dbb52efa2cf7061e469dc74e9e9035c
Dennis <dennis.dehiller@gmail.com>
2021-10-19 12:23:53 +0200

    Initial

--- ass3.py
+++ ass3.py
@@ -5,19 +5,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
+from statistics import NormalDist
 
 data = pd.read_excel('data_Ass3_G2.xlsx', sheet_name='Returns', engine='openpyxl').drop('date', 1)
 
-cov_matrix = data.iloc[:, 2:].cov()
-mean_vector = data.iloc[:, 2:].mean()
-weight_vector = pd.DataFrame({'w': ([0.2] * 5)})
-
-portfolio_my = weight_vector.values.flatten().T.dot(mean_vector)
-portfolio_sigma = (((weight_vector.values.flatten().T.dot(cov_matrix)).dot(weight_vector)) ** 0.5)[0]
-
-portfolio_value = 50000
-alpha = 0.98
+def ex1(data):
+    cov_matrix = data.iloc[:, 2:].cov()
+    mean_vector = data.iloc[:, 2:].mean()
+    weight_vector = pd.DataFrame({'w': ([0.2] * 5)})
 
 
+    portfolio_my = weight_vector.values.flatten().T.dot(mean_vector)
+    portfolio_sigma = (((weight_vector.values.flatten().T.dot(cov_matrix)).dot(weight_vector)) ** 0.5)[0]
 
-VaR = (-portfolio_my * portfolio_value) + (portfolio_sigma * portfolio_value * alpha)
+    portfolio_value = 50000
+
+    inv_dist_98 = NormalDist(0, 1).inv_cdf(0.98)
+
+    VaR = (-portfolio_my * portfolio_value) + (portfolio_sigma * portfolio_value * inv_dist_98)
+
+    # print ("My: ", portfolio_my)
+    # print ("Sigma: ", portfolio_sigma)
+    # print(VaR)
